5950d31d178efeb1d48b9ca16cccebc7db898d56
Justin Oeters <justin.oeters@gmail.com>
2022-01-08 12:05:39 -0500

    added Routes and base pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import MyGames from "./pages/MyGames";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/mygames" element={<MyGames />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Nav.js
@@ -0,0 +1,34 @@
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <NavLink className="nav-link" activeClassName="active" to="/login/">
+            Login
+          </NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink className="nav-link" activeClassName="active" to="/signup">
+            Signup
+          </NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink className="nav-link" activeClassName="active" to="/mygames">
+            My Games
+          </NavLink>
+        </li>
+        <li class="nav-item">
+          <NavLink className="nav-link" activeClassName="active" to="/">
+            Home
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
+
+<nav className="navbar"></nav>;
