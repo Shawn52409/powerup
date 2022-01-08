56bd49d7782f51791538c2b0f6f5e45d1e816d33
Shawn Miller <shawnsmm@gmail.com>
2022-01-08 13:13:11 -0500

    created models

---
+++ server/models/Game.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const gameSchema = new Schema({
+    gameName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    cover:{
+        type: String,
+        required: false,
+        trim: true
+    },
+    publisher: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    releaseDate: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    esrbRating:{
+        type: String,
+        required: false,
+        trim: true
+    },  
+    likes:{
+        type: String,
+        required: false,
+        trim: true
+    },  
+});
+
+const game = mongoose.model('Game', gameSchema);
+
+module.exports = game;
