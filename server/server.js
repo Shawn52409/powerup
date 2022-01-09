f51952fe42026e2c359c22296506a6c85e463678
Shawn Miller <shawnsmm@gmail.com>
2022-01-09 13:51:45 -0500

    added auth middleware

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
-require('.dotenv').config();
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const db = require('./config/connection');
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require("./utils/auth");
+
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware,
 });
 
 server.applyMiddleware({ app });
@@ -26,10 +29,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
+db.once("open", () => {
+  console.log("Connected to Database");
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    console.log("Connected to Database");
   });
 });
