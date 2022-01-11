bcd4619f5dedd62e54aa3fd46fd413250a3eb9e4
Ben Kasper <bckasper@gmail.com>
2022-01-11 18:57:48 -0500

    Pulled in bootstrap and initial CSS navbar changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,30 +4,30 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="navbar">
-      <ul className="nav nav-tabs">
-        <li className="nav-item">
-          <NavLink className="nav-link" activeClassName="active" to="/login/">
-            Login
-          </NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink className="nav-link" activeClassName="active" to="/signup">
-            Signup
-          </NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink className="nav-link" activeClassName="active" to="/mygames">
-            My Games
-          </NavLink>
-        </li>
-        <li class="nav-item">
-          <NavLink className="nav-link" activeClassName="active" to="/">
-            Home
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div className="d-flex container-fluid bg-dark">
+        <ul className="border navbar-custom nav justify-content-end">
+          <li className="nav-item ">
+            <NavLink className="nav-link" activeClassName="active" to="/login/">
+              Login
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink className="nav-link" activeClassName="active" to="/signup">
+              Signup
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink className="nav-link" activeClassName="active" to="/mygames">
+              My Games
+            </NavLink>
+          </li>
+          <li class="nav-item">
+            <NavLink className="nav-link" activeClassName="active" to="/">
+              Home
+            </NavLink>
+          </li>
+        </ul>
+    </div>
   );
 };
 
