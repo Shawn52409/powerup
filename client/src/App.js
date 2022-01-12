01327417f63b4eae0499329c8c77680fc56888ab
Ben <85649510+bckasper@users.noreply.github.com>
2022-01-11 21:17:55 -0500

    Revert "Signup saves"

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
-import SearchGames from "./pages/SearchGames";
-import SavedGames from "./pages/SavedGames";
-import Navbar from "./components/Navbar";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-
-
-
-const client = new ApolloClient({
-  request: (operation) => {
-    const token = localStorage.getItem("id_token");
-
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
-  uri: "/graphql",
-});
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={SearchGames} />
-            <Route exact path="/saved" component={SavedGames} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/login" component={Login} />
-            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
-    </ApolloProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
