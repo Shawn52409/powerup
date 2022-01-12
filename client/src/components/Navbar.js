122cdb0f75b568927eb5966ac4be624af6c1164a
Ben Kasper <bckasper@gmail.com>
2022-01-11 19:21:50 -0500

    Linked App.css to public HTML

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,8 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className="d-flex container-fluid bg-dark">
+    <div className="container-fluid bg-dark justify-content-center align-items-center py-2">
+      <h1 className="px-4 me-5 text-center">Power Up</h1>
         <ul className="border navbar-custom nav justify-content-end">
           <li className="nav-item ">
             <NavLink className="nav-link" activeClassName="active" to="/login/">
