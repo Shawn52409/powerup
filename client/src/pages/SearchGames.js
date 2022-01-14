bdb5940f7190877226b9551076706a1a3bce6b37
Justin Oeters <justin.oeters@gmail.com>
2022-01-14 14:47:21 -0500

    connected cards to db to auto populate

--- client/src/pages/SearchGames.js
+++ client/src/pages/SearchGames.js
@@ -1,12 +1,59 @@
 import React from "react";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import { GET_ME, GET_ALL_GAMES, GET_GAME } from "../utils/queries";
+import { Container, CardColumns, Card, Button } from "react-bootstrap";
 
 const SearchGames = () => {
   const { loading, data, error } = useQuery(GET_ALL_GAMES);
+  console.log(error, loading, data);
+  if (loading) return <div>this page is loading!!!!!!!!!!!</div>;
 
-  console.log(loading, data, error);
-  return <h1>This is the search game tab</h1>;
+  if (error) return <div>something went wrong</div>;
+  return (
+    <Container>
+      <h2>
+        Search Bar
+        {/* {searchedGames.length
+          ? `Viewing ${searchedGames.length} results:`
+          : "Search for a game to begin"} */}
+      </h2>
+      <CardColumns>
+        {data.game.map((game) => {
+          return (
+            <Card key={game._id} border="dark">
+              {game.cover ? (
+                <Card.Img
+                  src={game.cover}
+                  alt={`The cover for ${game.gameName}`}
+                  variant="top"
+                />
+              ) : null}
+              <Card.Body>
+                <Card.Title>{game.gameName}</Card.Title>
+                <p className="small">Year: {game.year}</p>
+                <Card.Text>{game.description}</Card.Text>
+                {/* {Auth.loggedIn() && (
+                  <Button
+                    disabled={savedGameIds?.some(
+                      (savedGameId) => savedGameId === game.gameId
+                    )}
+                    className="btn-block btn-info"
+                    onClick={() => handleSaveGame(game.gameId)}
+                  >
+                    {savedGameIds?.some(
+                      (savedGameId) => savedGameId === game.gameId
+                    )
+                      ? "This game has been saved!"
+                      : "Save this Game!"}
+                  </Button>
+                )} */}
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </CardColumns>
+    </Container>
+  );
 };
 
 export default SearchGames;
