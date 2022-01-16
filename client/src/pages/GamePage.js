1ec1451b1c6b625045ff520a68b714692f007c71
Shawn Miller <shawnsmm@gmail.com>
2022-01-16 12:01:21 -0500

    added pretty array function to gamepage

--- client/src/pages/GamePage.js
+++ client/src/pages/GamePage.js
@@ -1,48 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/react-hooks";
-import { GET_ONE_GAME } from "../utils/queries";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { SAVE_GAME } from '../utils/mutations';
+import { GET_ME, GET_ONE_GAME } from "../utils/queries";
 import { Container, Row, Button } from "react-bootstrap";
 import Auth from "../utils/auth";
+import PrettyArray from "../utils/prettyArray";
+
 
 const GamePage = () => {
   const { _id: gameParam } = useParams();
   const { data } = useQuery(GET_ONE_GAME, {
     variables: { _id: gameParam },
   });
+  
 
-  // const handleSaveGame = async (gameId) => {
-  //   // find the game in `searchedGames` state by the matching id
-  //   const gameToSave = searchedGames.find((game) => game.gameId === gameId);
-
-  //   // get token
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //   if (!token) {
-  //     return false;
-  //   }
-
-  //   try {
-  //     await saveGame({
-  //       variables: { game: gameToSave },
-  //       update: (cache) => {
-  //         const { me } = cache.readQuery({ query: GET_ME });
-  //         // console.log(me)
-  //         // console.log(me.savedGames)
-  //         cache.writeQuery({
-  //           query: GET_ME,
-  //           data: { me: { ...me, savedGames: [...me.savedGames, gameToSave] } },
-  //         });
-  //       },
-  //     });
-
-  //     // if game successfully saves to user's account, save game id to state
-  //     setSavedGameIds([...savedGameIds, gameToSave.gameId]);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-  // If no data then wait for data
   if (!data) {
     return <div>Loading...</div>;
   }
@@ -62,15 +34,15 @@ const GamePage = () => {
             <h1 className="text-center">{data.getOneGame.gameName}</h1>
             <h2 className="text-center">{data.getOneGame.year}</h2>
             <p className="">
-              <b>Genre:</b> {data.getOneGame.genre}
+              <b>Genre:</b> {PrettyArray.printArray(data.getOneGame.genre)}
             </p>
             <p className="">
-              <b>Platforms:</b> {data.getOneGame.platform}
+              <b>Platforms:</b> {PrettyArray.printArray(data.getOneGame.platform)}
             </p>
             <p className="">
               <b>Description:</b> {data.getOneGame.description}
             </p>
-            {Auth.loggedIn() && <Button> Save Game</Button>}
+            {Auth.loggedIn() && <Button > Save Game</Button>}
           </div>
         </div>
       </Row>
