501352e59d4d916f3b9482e1bb72802c7dce3c9a
Shawn Miller <shawnsmm@gmail.com>
2022-01-16 15:59:42 -0500

    updated user seed file

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,13 @@
 const db = require('../config/connection');
-const { Game, User } = require('../models');
-const gameSeeds = require('./gameSeeds.json');
+// const { Game, User } = require('../models');
+const { User } = require('../models');
+// const gameSeeds = require('./gameSeeds.json');
 const userSeeds = require('./userSeeds.json');
 
 db.once('open', async () => {
   try {
-    await Game.deleteMany({});
-    await Game.create(gameSeeds);
+    // await Game.deleteMany({});
+    // await Game.create(gameSeeds);
     await User.deleteMany({});
     await User.create(userSeeds);
 
